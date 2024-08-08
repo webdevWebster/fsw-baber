bfe471eebc7f22b997adc6500fa9fbd5dfd174ae
webstr ribeiro <websterdesignribeiro@gmail.com>
2024-08-07 22:09:46 -0300

    chore: add husky and lint-staged setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { Button } from "./_components/ui/button";
-
 export default function Home() {
   return (
-    <Button>Teste</Button>
-  );
+    <div>
+      <h1 className="mb-3 pt-11 text-red-400">Aqui está</h1>
+    </div>
+  )
 }
