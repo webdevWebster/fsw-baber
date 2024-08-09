dc1191571aaca61a76e904e595851b6dbfb4e4b8
webstr ribeiro <websterdesignribeiro@gmail.com>
2024-08-08 22:03:13 -0300

    chore: add header and banner in the front

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
+
 export default function Home() {
   return (
     <div>
-      <h1 className="mb-3 pt-11 text-red-400">Aqui está</h1>
+      <Header />
+      <div className="p-5">
+        <h1 className="text-xl font-bold">Olá Webster!</h1>
+        <p>Segunda-feira, 5 de agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/Banner.png"
+            fill
+            className="rounded-xl object-cover"
+            alt="banner"
+          />
+        </div>
+      </div>
     </div>
   )
 }
